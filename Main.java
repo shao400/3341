e375c3b6c58981774c6d8721073195ec470cb249
Zilin Shao <66044248+shao400@users.noreply.github.com>
2021-05-22 09:19:39 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
-class Main {
-	public static void main(String[] args) {
-		// Initialize the scanner with the input file
-		Scanner S = new Scanner(args[0]);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+class Main {
+	public static void main(String[] args) throws IOException {
+		// Initialize the scanner with the input file
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter your file: ");
+		 
+		String input = reader.readLine();
+		System.out.println("Your file is: " + input);
+		
+		ScannerX S = new ScannerX(input);
+		//Scanner S = new Scanner(args[0]);
+		//System.out.println(S.coreSize());
+		//System.out.println(S.tokenSize());
 		// Print the token stream
+		//S.currentToken() != Core.EOF && S.currentToken() != Core.ERROR
+		//int i = 0;
 		while (S.currentToken() != Core.EOF && S.currentToken() != Core.ERROR) {
 			// Pring the current token, with any extra data needed
 			System.out.print(S.currentToken());
@@ -18,6 +32,7 @@ class Main {
 
 			// Advance to the next token
 			S.nextToken();
+			//i++;
 		}
 	}
 }
